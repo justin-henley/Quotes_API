7ddafa7443a714ba42007ca32eb54d4978c666f5
justin-henley <justin.a.henley@gmail.com>
2022-03-03 02:47:01 +0800

    Wrong code in file

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -2,8 +2,6 @@
 // Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/Database.php';
 include_once '../../models/Author.php';
@@ -15,27 +13,37 @@ $connection = $database->connect();
 // Instantiate author object
 $author = new Author($connection);
 
-// Get raw POST data
-$data = json_decode(file_get_contents("php://input"));
+// Get ID from URL
+$author->id = isset($_GET['id'])
+    ? $_GET['id']
+    : null;
 
-// Get author name from data
-$author->author = $data->author;
-
-// Attempt to create
-if (empty($author->author)) {
-    // Missing parameters
+// TODO this is ugly
+// Return early if parameters missing;
+if (empty($author->id)) {
     echo json_encode([
         'message' => 'Missing Required Parameters'
     ]);
-} else if ($author->create()) {
-    // Create operation successful
-    echo json_encode([
-        'id' => $connection->lastInsertId(),
+    return;
+}
+
+// Get author data
+$author->readSingle();
+
+// Check if a author was returned for the specified ID (author exists in database)
+if ($author->author) {
+    // Read operation found a author
+    // Create results array
+    $catArr = [
+        'id' => $author->id,
         'author' => $author->author,
-    ]);
+    ];
+
+    // Convert to JSON and output 
+    echo (json_encode($catArr));
 } else {
-    // Create operation failed
+    // Read operation did not find a author
     echo json_encode([
-        'message' => 'Author Not Created'
+        'message' => 'authorId Not Found'
     ]);
 }
