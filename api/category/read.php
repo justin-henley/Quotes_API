22b8ab2e8f8cddfe19f7e5e485af935fcaf57a92
justin-henley <justin.a.henley@gmail.com>
2022-03-02 21:24:53 +0800

    testing print_r vs echo

--- api/category/read.php
+++ api/category/read.php
@@ -36,8 +36,8 @@ if ($result->rowCount() > 0) {
     }
 
     // Turn to JSON and output
-    echo json_encode($catArr);
+    print_r(json_encode($catArr));
 } else {
     // No categories found
-    echo json_encode(['message' => 'No Categories Found']);
+    print_r(json_encode(['message' => 'No Categories Found']));
 }
