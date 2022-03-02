de73304234c180d009eb5ddc031831a8cb4f376a
justin-henley <justin.a.henley@gmail.com>
2022-03-02 21:16:16 +0800

    read_single written

---
+++ api/category/read_single.php
@@ -0,0 +1,31 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+// Instantiate new DB and connect
+$database = new Database();
+$connection = $database->connect();
+
+// Instantiate Category object
+$category = new Category($connection);
+
+// Get ID from URL
+$category->id = isset($_GET['id'])
+    ? $_GET['id']
+    : die();
+
+// Get category data
+$category->readSingle();
+
+// Create results array
+$catArr = [
+    'id' => $category->id,
+    'category' => $category->category,
+];
+
+// Convert to JSON and output
+print_r(json_encode($catArr));
