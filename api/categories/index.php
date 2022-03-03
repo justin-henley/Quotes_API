a8f3588e9a8112531694e93ebdf102aa17424aad
justin-henley <justin.a.henley@gmail.com>
2022-03-04 03:23:29 +0800

    typo was causing the CORS check to fail

--- api/categories/index.php
+++ api/categories/index.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json');
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 // CORS solution
-if ($method === 'OPTIONS') {
+if ($requestMethod === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
 }
