3507a62f4e733c3afb6b27227d89d759dccff374
justin-henley <justin.a.henley@gmail.com>
2022-03-04 03:06:27 +0800

    Does allowing all methods allow test site to run?

--- api/authors/index.php
+++ api/authors/index.php
@@ -2,6 +2,7 @@
 // Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 // Get the request method
 $requestMethod = $_SERVER['REQUEST_METHOD'];
