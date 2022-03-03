a77e64f7cf3daa4f382c4603612ee480676457a3
justin-henley <justin.a.henley@gmail.com>
2022-03-04 03:02:21 +0800

    endpoint switching

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -11,3 +11,24 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
 }
+
+// Choose the operation to carry out
+switch ($requestMethod) {
+    case 'GET':
+        // If an id was provided, read a single quote. Otherwise read all quotes
+        if (isset($_GET['id'])) {
+            require './read_single.php';
+        } else {
+            require './read.php';
+        }
+        break;
+    case 'POST':
+        require './create.php';
+        break;
+    case 'PUT':
+        require './update.php';
+        break;
+    case 'DELETE':
+        require './delete.php';
+        break;
+}
