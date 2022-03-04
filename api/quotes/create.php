3fc5cb15de79b67e7b62244b9cacdb3b2a1bfac2
justin-henley <justin.a.henley@gmail.com>
2022-03-04 21:30:23 +0800

    checking for existing author and category - stubs created

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -7,6 +7,9 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type
 
 include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
+include_once '../../models/Author.php';
+include_once '../../models/Category.php';
+
 
 // Instantiate new DB and connect
 $database = new Database();
@@ -25,7 +28,7 @@ $quote->categoryId = $data->categoryId;
 
 // TODO when to check if the table has that author and id? Should fail to create with a message
 
-// Attempt to create
+/* // Attempt to create
 if (empty($quote->quote) || empty($quote->authorId) || empty($quote->categoryId)) {
     // Missing parameters
     echo json_encode([
@@ -45,4 +48,72 @@ if (empty($quote->quote) || empty($quote->authorId) || empty($quote->categoryId)
     echo json_encode([
         'message' => 'Quote Not Created'
     ]);
+} */
+
+// TODO alt
+
+// Attempt to create
+
+
+if (empty($quote->quote) || empty($quote->authorId) || empty($quote->categoryId)) {
+    // Missing parameters
+    echo json_encode([
+        'message' => 'Missing Required Parameters'
+    ]);
+} else {
+    // All parameters provided, but not guaranteed to exist in DB
+    if (!authorExists($quote->authorId, $connection)) {
+        // Author not found
+        echo json_encode([
+            'message' => 'authorId Not Found'
+        ]);
+    } else if (!categoryExists($quote->categoryId, $connection)) {
+        // Category not found
+        echo json_encode([
+            'message' => 'categoryId Not Found'
+        ]);
+    } else if ($quote->create()) {
+        // Category and Author both exist
+        // Create operation successful
+        echo json_encode([
+            'id' => $connection->lastInsertId(),
+            'quote' => $quote->quote,
+            'authorId' => $quote->authorId,
+            'categoryId' => $quote->categoryId,
+        ]);
+    } else {
+        // Create operation failed
+        echo json_encode([
+            'message' => 'Quote Not Created'
+        ]);
+    }
+}
+
+
+/**
+ * Function to check if an authorId exists in the database
+ * 
+ * @param {int} authorId - The author ID to check
+ * @param {PDO} connection - An existing PDO database connection
+ * @return {boolean} - True if author exists in database
+ */
+
+function authorExists($authorId, $connection)
+{
+    // TODO
+    return false;
+}
+
+/**
+ * Function to check if an categoryId exists in the database
+ * 
+ * @param {int} categoryId - The author ID to check
+ * @param {PDO} connection - An existing PDO database connection
+ * @return {boolean} - True if author exists in database
+ */
+
+function categoryExists($categoryId, $connection)
+{
+    // TODO
+    return false;
 }
