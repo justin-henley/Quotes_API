79445be09d8255f6cb7820a834d0a9513cf6d7f8
justin-henley <justin.a.henley@gmail.com>
2022-03-04 22:43:57 +0800

    added author and category checks to update

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -8,8 +8,7 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type
 include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
 
-// TODO doesn't update author or category
-// MUST be able to update all fields but id
+// TODO check for valid author and category
 
 // Instantiate new DB and connect
 $database = new Database();
@@ -28,12 +27,25 @@ $quote->authorId = $data->authorId;
 $quote->categoryId = $data->categoryId;
 
 // Attempt to update
+// Check all parameters are non-null, and category and author exist in the database
+
 if (empty($quote->id) || empty($quote->quote) || empty($quote->authorId) || empty($quote->categoryId)) {
     // Missing parameters
     echo json_encode([
         'message' => 'Missing Required Parameters'
     ]);
+} else if (!$quote->authorExists()) {
+    // Author not found in database
+    echo json_encode([
+        'message' => 'authorId Not Found'
+    ]);
+} else if (!$quote->categoryExists()) {
+    // Category not found in database
+    echo json_encode([
+        'message' => 'categoryId Not Found'
+    ]);
 } else if ($quote->update()) {
+    // Category and Author both exist
     // Update operation successful
     echo json_encode([
         'id' => $quote->id,
